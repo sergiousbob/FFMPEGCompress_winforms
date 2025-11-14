171878af4cdae513c696bc2db708d90bd9ac19f5
caesa <caesa@DESKTOP-4828I1A>
2025-11-14 20:00:13 +0500

    Добавьте файлы проекта.

---
+++ FFMPEGCompress/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FFMPEGCompress
+{
+    public partial class Form1 : Form
+    {
+        int crf_video;
+        static Form1 form1;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = "lossless";
+            this.BackColor = Color.Aquamarine;
+        }
+
+
+
+
+        void asyncBtn()
+        {
+            int a = 1;
+
+
+            if (comboBox1.SelectedItem == "lossless")
+            {
+                crf_video = 17;
+        
+
+            }
+            if (comboBox1.SelectedItem == "default")
+            {
+                crf_video = 23;
+        
+
+            }
+            if (comboBox1.SelectedItem == "worst")
+            {
+                crf_video = 28;
+        
+
+            }
+
+
+
+            string directoryPath = $"{textBox1.Text}";
+            string directoryOut = $@"{textBox2.Text}";
+            string[] aviFiles = Directory.GetFiles(directoryPath, "*.avi");
+  
+
+            foreach (string aviFile in aviFiles)
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "ffmpeg";
+
+
+
+
+
+                string naimen = $@"{textBox2.Text}\VideoConvert{a}";
+         
+
+
+                process.StartInfo.Arguments = $"-i {aviFile} -vcodec libx265 -crf {crf_video} -preset faster {naimen}.mp4";
+                a++;
+
+
+
+                process.StartInfo.UseShellExecute = false;
+
+
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+                process.WaitForExit();
+
+
+
+                string output = process.StandardOutput.ReadToEnd();
+ 
+            }
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => asyncBtn());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Выберите путь к папке" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+
+                    textBox1.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Выберите путь к папке" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+
+                    textBox2.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure wanna be exit ?", "Exit Application", MessageBoxButtons.YesNoCancel);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                
+                Process[] p = Process.GetProcessesByName("ffmpeg");
+                if (p.Length > 0) p[0].Kill();
+                Process[] x = Process.GetProcessesByName("FFMPEGCompress");
+                if (x.Length > 0) x[0].Kill();
+            }
+            else
+            {
+
+                e.Cancel = true;
+
+            }
+
+
+     
+
+
+        }
+    }
+}
---
+++ FFMPEGCompress/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("FFMPEGCompress")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FFMPEGCompress")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5f24c8d7-430f-4b69-9c11-c5a19967726f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
